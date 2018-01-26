8c743915ad4340d62e0bc00acf969fa513a4f48f
Anchit <anc.shukla@gmail.com>
2018-01-26 19:26:37 +0530

    Setup source maps for css

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = (env) => {
   const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
 
   return {
     entry: './src/app.js',
@@ -17,15 +19,20 @@ module.exports = (env) => {
         exclude: /node_modules/,
       }, {
         test: /\.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
+        use: CSSExtract.extract({
+          use: [
+            'css-loader',
+            'sass-loader'
+          ]
+        })
       }]
     },
 
-    devtool: isProduction ? 'source-map' : 'cheap-module-eval-suurce-map',
+    plugins: [
+      CSSExtract
+    ],
+
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
 
     devServer: {
       contentBase: path.join(__dirname,'public'),
